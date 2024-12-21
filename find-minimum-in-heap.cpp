caaa71ccb26f83452667d22b43f2036363e8654c
bhushan-madankar <bmadankar0@gmail.com>
2024-12-21 12:00:00 +0530

    Added find-minimum-in-heap.cpp

--- find-minimum-in-heap.cpp
+++ find-minimum-in-heap.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 using namespace std;
 int main() {
+// activity chunk 36
     priority_queue<int, vector<int>, greater<int>> pq;
     pq.push(4); pq.push(2); pq.push(7);
     cout << pq.top();
