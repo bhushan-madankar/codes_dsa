99933a7cc2562f7401d85a2be16c942c09076e88
bhushan-madankar <bmadankar0@gmail.com>
2025-01-07 12:00:00 +0530

    Added find-minimum-in-linked-list.cpp

---
+++ find-minimum-in-linked-list.cpp
@@ -0,0 +1,20 @@
+// Find minimum in linked list
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* next;
+    Node(int v) : data(v), next(nullptr) {}
+};
+int findMin(Node* head) {
+    int mn = head->data;
+    while (head) { mn = min(mn, head->data); head = head->next; }
+    return mn;
+}
+int main() {
+    Node* head = new Node(7);
+    head->next = new Node(3);
+    head->next->next = new Node(9);
+    cout << findMin(head);
+    return 0;
+}
