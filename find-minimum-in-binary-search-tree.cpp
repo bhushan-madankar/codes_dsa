4d60aeb5c1d344f6282dcf73b7ad9765e4369985
bhushan-madankar <bmadankar0@gmail.com>
2025-01-08 12:00:00 +0530

    Added find-minimum-in-binary-search-tree.cpp

---
+++ find-minimum-in-binary-search-tree.cpp
@@ -0,0 +1,20 @@
+// Find minimum in BST (recursive)
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int v) : data(v), left(nullptr), right(nullptr) {}
+};
+int findMin(Node* root) {
+    if (!root->left) return root->data;
+    return findMin(root->left);
+}
+int main() {
+    Node* root = new Node(12);
+    root->left = new Node(7);
+    root->right = new Node(15);
+    cout << findMin(root);
+    return 0;
+}
