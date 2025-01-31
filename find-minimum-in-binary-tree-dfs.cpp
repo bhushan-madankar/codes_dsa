bc1f5b4ae647b94cbef1114ee6287695a331b8a1
bhushan-madankar <bmadankar0@gmail.com>
2025-01-31 12:00:00 +0530

    Added find-minimum-in-binary-tree-dfs.cpp

---
+++ find-minimum-in-binary-tree-dfs.cpp
@@ -0,0 +1,29 @@
+// Find minimum in binary tree (DFS)
+#include <iostream>
+#include <stack>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int v) : data(v), left(nullptr), right(nullptr) {}
+};
+int findMin(Node* root) {
+    int mn = INT_MAX;
+    stack<Node*> st;
+    st.push(root);
+    while (!st.empty()) {
+        Node* n = st.top(); st.pop();
+        mn = min(mn, n->data);
+        if (n->left) st.push(n->left);
+        if (n->right) st.push(n->right);
+    }
+    return mn;
+}
+int main() {
+    Node* root = new Node(19);
+    root->left = new Node(10);
+    root->right = new Node(24);
+    cout << findMin(root);
+    return 0;
+}
