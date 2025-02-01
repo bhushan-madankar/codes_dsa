55a9bc34059ee1e17be5ac9cb4a035ba3c0c66d1
bhushan-madankar <bmadankar0@gmail.com>
2025-02-01 12:00:00 +0530

    Added find-minimum-in-binary-tree-inorder.cpp

---
+++ find-minimum-in-binary-tree-inorder.cpp
@@ -0,0 +1,25 @@
+// Find minimum in binary tree (inorder)
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int v) : data(v), left(nullptr), right(nullptr) {}
+};
+void inorder(Node* root, int& mn) {
+    if (!root) return;
+    inorder(root->left, mn);
+    mn = min(mn, root->data);
+    inorder(root->right, mn);
+}
+int main() {
+    Node* root = new Node(15);
+    root->left = new Node(8);
+    root->right = new Node(18);
+    int mn = INT_MAX;
+    inorder(root, mn);
+    cout << mn;
+    return 0;
+}
