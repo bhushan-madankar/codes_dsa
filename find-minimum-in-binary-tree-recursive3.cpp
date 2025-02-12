ba62fc20554a65bbc277c0c8c448f134cb105879
bhushan-madankar <bmadankar0@gmail.com>
2025-02-12 12:00:00 +0530

    Added find-minimum-in-binary-tree-recursive3.cpp

---
+++ find-minimum-in-binary-tree-recursive3.cpp
@@ -0,0 +1,23 @@
+// Find minimum in binary tree (recursive, another)
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int v) : data(v), left(nullptr), right(nullptr) {}
+};
+int findMin(Node* root) {
+    if (!root) return INT_MAX;
+    int l = findMin(root->left);
+    int r = findMin(root->right);
+    return min(root->data, min(l, r));
+}
+int main() {
+    Node* root = new Node(27);
+    root->left = new Node(18);
+    root->right = new Node(32);
+    cout << findMin(root);
+    return 0;
+}
