04fe385fa91ddc5b121cbf2ba8a3e23d17744d0c
bhushan-madankar <bmadankar0@gmail.com>
2025-02-12 12:00:00 +0530

    Added find-minimum-in-bst-recursive.cpp

---
+++ find-minimum-in-bst-recursive.cpp
@@ -0,0 +1,20 @@
+// Find minimum in BST recursively
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int v) : data(v), left(nullptr), right(nullptr) {}
+};
+int findMin(Node* root) {
+    if (!root->left) return root->data;
+    return findMin(root->left);
+}
+int main() {
+    Node* root = new Node(8);
+    root->left = new Node(3);
+    root->right = new Node(10);
+    cout << findMin(root);
+    return 0;
+}
