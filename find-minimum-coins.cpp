a7d4f23334dfa3c2a7bc869371766fe5c18293cf
bhushan-madankar <bmadankar0@gmail.com>
2025-02-13 12:00:00 +0530

    Added find-minimum-coins.cpp

--- find-minimum-coins.cpp
+++ find-minimum-coins.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int coinChange(vector<int>& coins, int amount) {
     vector<int> dp(amount+1, INT_MAX);
     dp[0] = 0;
+// activity chunk 32
     for (int i = 1; i <= amount; ++i)
         for (int c : coins)
             if (i >= c && dp[i-c] != INT_MAX)
